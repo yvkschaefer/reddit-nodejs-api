ca914964ccc5b8cb9a553783603c2c2a269aa6a1
yvkschaefer <yvkschaefer@gmail.com>
2016-07-21 18:55:33 +0000

    added foreign key to posts table, subredditId

--- reddit.sql
+++ reddit.sql
@@ -35,3 +35,31 @@ CREATE TABLE `subreddits` (
   PRIMARY KEY (`id`), 
   UNIQUE KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+mysql> ALTER TABLE posts ADD COLUMN subredditId INT(11) NOT NULL;                                     
+Query OK, 2 rows affected (0.03 sec)
+Records: 2  Duplicates: 0  Warnings: 0
+
+mysql> truncate posts;
+Query OK, 0 rows affected (0.01 sec)
+
+mysql> ALTER TABLE posts ADD FOREIGN KEY (subredditId) REFERENCES subreddits(id);
+Query OK, 0 rows affected (0.04 sec)
+Records: 0  Duplicates: 0  Warnings: 0
+
+mysql> describe posts;
++-------------+---------------+------+-----+---------+----------------+
+| Field       | Type          | Null | Key | Default | Extra          |
++-------------+---------------+------+-----+---------+----------------+
+| id          | int(11)       | NO   | PRI | NULL    | auto_increment |
+| title       | varchar(300)  | YES  |     | NULL    |                |
+| url         | varchar(2000) | YES  |     | NULL    |                |
+| userId      | int(11)       | YES  | MUL | NULL    |                |
+| createdAt   | datetime      | NO   |     | NULL    |                |
+| updatedAt   | datetime      | NO   |     | NULL    |                |
+| subredditId | int(11)       | NO   | MUL | NULL    |                |
++-------------+---------------+------+-----+---------+----------------+
+7 rows in set (0.00 sec)
