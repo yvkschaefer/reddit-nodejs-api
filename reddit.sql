e4f6a6ffa024f6fff7c258dcef230c8119183a98
yvkschaefer <yvkschaefer@gmail.com>
2016-07-21 00:06:59 +0000

    started step one of subreddits table

--- reddit.sql
+++ reddit.sql
@@ -23,3 +23,25 @@ CREATE TABLE `posts` (
   KEY `userId` (`userId`),
   CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `subreddits` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `createdAt` TIMESTAMP,
+  `updatedAt` TIMESTAMP,
+  UNIQUE (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+mysql> describe subreddits;
++-------------+--------------+------+-----+---------------------+-----------------------------+
+| Field       | Type         | Null | Key | Default             | Extra                       |
++-------------+--------------+------+-----+---------------------+-----------------------------+
+| id          | int(11)      | NO   | PRI | NULL                | auto_increment              |
+| name        | varchar(30)  | NO   |     | NULL                |                             |
+| description | varchar(200) | YES  |     | NULL                |                             |
+| createdAt   | timestamp    | NO   |     | CURRENT_TIMESTAMP   | on update CURRENT_TIMESTAMP |
+| updatedAt   | timestamp    | NO   |     | 0000-00-00 00:00:00 |                             |
++-------------+--------------+------+-----+---------------------+-----------------------------+
+5 rows in set (0.00 sec)
